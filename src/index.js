8779a18b572a22f19650775e6b21bd6a087493a9
chipmunkcol <jack95711@gmail.com>
2022-09-05 12:03:36 +0900

    navbar 러프하게 작성 / /login만 연결

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from "react-redux"
 import store from './Redux/config/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-      <BrowserRouter>
-
+      <Router>
           <App />
-
-      </BrowserRouter>
+      </Router>
   </Provider>
 );
 
