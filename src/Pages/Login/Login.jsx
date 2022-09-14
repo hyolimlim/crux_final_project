db8d5a4de02b467a875d2f0f914339060fb7cdd1
hyolimLim <hyolimlim.dev@gmail.com>
2022-09-14 20:22:30 +0900

    회원가입, 크루 생성 구현

--- src/Pages/Login/Login.jsx
+++
@@ -1,9 +0,0 @@
-
-const Login = () => {
-    return(
-        <div>
-            <h1>로그인 화면입니다</h1>
-        </div>
-    );
-}
-export default Login;
--- src/Redux/modules/userSlice.js
+++ src/Redux/modules/userSlice.js
@@ -13,6 +13,40 @@ const initialState = {
   error: null,
 };
 
+//회원가입
+export const signup = createAsyncThunk(
+  "/members/signup",
+  async (payload, thunkAPI) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/members/signup`, payload);
+      window.alert("회원가입 성공");
+      window.location.replace("/");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.data);
+    }
+  }
+);
+
+export const signupSlice = createSlice({
+  name: "signup",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [signup.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [signup.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+    },
+    [signup.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
 //로그인
 export const login = createAsyncThunk(
   "members/login",
@@ -27,27 +61,6 @@ export const login = createAsyncThunk(
             response.headers.access_token
           );
           window.location.replace("/");
-        });
-      return thunkAPI.fulfillWithValue(response.data);
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.data);
-    }
-  }
-);
-
-//카카오톡 로그인
-export const kakaoLogin = createAsyncThunk(
-  "members/kakaoLogin",
-  async (payload, thunkAPI) => {
-    try {
-      const response = await axios
-        .get(`http://3.39.237.124/oauth/kakao/callback?code=${payload}`)
-        .then((response) => {
-          console.log(response);
-          window.localStorage.setItem(
-            "access_token",
-            response.headers.access_token
-          );
         });
       return thunkAPI.fulfillWithValue(response.data);
     } catch (error) {
@@ -76,25 +89,45 @@ export const loginSlice = createSlice({
   },
 });
 
+//카카오톡 로그인
+export const kakaoLogin = createAsyncThunk(
+  "members/kakaoLogin",
+  async (payload, thunkAPI) => {
+    try {
+      const response = await axios
+        .get(`http://3.39.237.124/oauth/kakao/callback?code=${payload}`)
+        .then((response) => {
+          console.log(response);
+          window.localStorage.setItem(
+            "access_token",
+            response.headers.access_token
+          );
+        });
+      return thunkAPI.fulfillWithValue(response.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.data);
+    }
+  }
+);
+
 export const kakaoLoginSlice = createSlice({
   name: "kakaoLogin",
   initialState,
   reducers: {},
   extraReducers: {
     [kakaoLogin.pending]: (state) => {
-      state.loading = true;
-      state.error = null;
+      state.isLoading = true;
     },
-    [kakaoLogin.fulfilled]: (state, { payload }) => {
-      state.loading = false;
-      state.success = true;
-      state.user = payload;
+    [kakaoLogin.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.user = action.payload;
     },
-    [kakaoLogin.rejected]: (state, { payload }) => {
+    [kakaoLogin.rejected]: (state, action) => {
       state.loading = false;
-      state.error = payload;
+      state.error = action.payload;
     },
   },
 });
 
-export default { loginSlice, kakaoLoginSlice }.reducer;
+export default { signupSlice, loginSlice, kakaoLoginSlice }.reducer;
